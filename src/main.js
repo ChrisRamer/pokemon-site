be5eebdf77880fcc5712f5ac67009ad67f2b9131
Chris Ramer <chrisramer.97@gmail.com>
2022-03-16 15:51:36 -0700

    Add click events, store pokemon to array, get 2 random pokemon for battle
    Co-authored-by: lucianmiller <lucianmiller@users.noreply.github.com>
    Co-authored-by: kiernan2 <kiernan1994@gmail.com>
    Co-authored-by: geomio <geomio@users.noreply.github.com>
    Co-authored-by: TJEverett <TJEverett@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -5,46 +5,69 @@ import './css/styles.css';
 import Pokemon from './js/pokemon';
 import PokemonService from './js/pokemon-service';
 
-function getElements(response) {
-  console.log(response);
-}
-
 async function makePokedexCall(region) {
   const response = await PokemonService.getPokedex(region);
-  getElements(response);
+  return response;
 }
 
-async function makePokemonCall(name) {
-	const response = await PokemonService.getPokemon(name);
-	getElements(response);
-}
+// async function makePokemonCall(name) {
+// 	const response = await PokemonService.getPokemon(name);
+// 	return response;
+// }
 
 async function newPokemonCreate(name) {
   const response = await PokemonService.getPokemon(name);
   let pokemon = new Pokemon(response, 1);
-  getElements(pokemon);
+  return pokemon;
 }
 
-async function randomPokemonCall(region) {
-  const response = await PokemonService.randomPokemon(region);
-  getElements(response);
+// async function randomPokemonCall(region) {
+//   const response = await PokemonService.randomPokemon(region);
+//   return response;
+// }
+
+// async function getMoveData(name) {
+//   const response = await PokemonService.getMove(name);
+//   return response;
+// }
+
+// async function getTypeData(type) {
+//   const response = await PokemonService.getType(type);
+//   return response;
+// }
+
+function getRandomNumber(min, max) {
+	return Math.ceil(Math.random() * ((max - min) + 1) + min);
 }
 
-async function getMoveData(name) {
-  const response = await PokemonService.getMove(name);
-  getElements(response);
+function createBattleObject() {
+	// Gets random pokemon object
+	let randNum = getRandomNumber(0, pokemonList.length - 1);
+	let randPokemon = pokemonList[randNum];
+	newPokemonCreate(randPokemon).then((playerPokemon) => {
+		randNum = getRandomNumber(0, pokemonList.length - 1);
+		randPokemon = pokemonList[randNum];
+		newPokemonCreate(randPokemon).then((opposingPokemon) => {
+			console.log(`${capitalizeFirst(playerPokemon["name"])} entered fight with ${capitalizeFirst(opposingPokemon["name"])}!`);
+		});
+	});
 }
 
-async function getTypeData(type) {
-  const response = await PokemonService.getType(type);
-  getElements(response);
+function capitalizeFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+//Store list of pokemon by region
+let pokemonList = [];
+makePokedexCall(1).then((response) => {
+  response["pokemon_entries"].forEach((pokemon) => {
+    pokemonList.push(pokemon["entry_number"]);
+  });
+});
 
 $(document).ready(function () {
-  getTypeData("normal");
-  makePokedexCall(2); // TODO:  Use configurable region
-	makePokemonCall("bulbasaur");
-  randomPokemonCall("2");
-  newPokemonCreate("bulbasaur");
-  getMoveData("pound")
+  $("#fight").on("click", function() {createBattleObject();});
+  $("#run").on("click", function () {console.log("run!");});
+  $("#pokemon").on("click", function() {console.log("party!");});
+  $("#items").on("click", function() {console.log("items!");});
 });
